048b67a87d408929dedb764505b418dd91d27539
Yezza <=>
2025-04-09 09:31:47 +0800

    Fix flake8 linting errors and update workflow

--- test_even_or_odd.py
+++ test_even_or_odd.py
@@ -1,13 +1,19 @@
+
+
 import unittest
 from even_or_odd import is_even
 
+
 class TestEvenOrOdd(unittest.TestCase):
+
+
     def test_is_even(self):
         self.assertTrue(is_even(2))
         self.assertTrue(is_even(0))
         self.assertFalse(is_even(1))
         self.assertFalse(is_even(-1))
         self.assertTrue(is_even(-2))
+
 
 if __name__ == '__main__':
     unittest.main()
