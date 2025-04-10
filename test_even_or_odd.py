b3b7e8c9310129d10fcc3fd152337cd51c883685
Yezza <=>
2025-04-10 22:52:46 +0800

    update

--- test_even_or_odd.py
+++ test_even_or_odd.py
@@ -1,18 +1,11 @@
 
 
-import unittest
 from even_or_odd import is_even
 
 
-class TestEvenOrOdd(unittest.TestCase):
-
-    def test_is_even(self):
-        self.assertTrue(is_even(2))
-        self.assertTrue(is_even(0))
-        self.assertFalse(is_even(1))
-        self.assertFalse(is_even(-1))
-        self.assertTrue(is_even(-2))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_is_even():
+    assert is_even(2)
+    assert is_even(0)
+    assert not is_even(1)
+    assert not is_even(-1)
+    assert is_even(-2)
